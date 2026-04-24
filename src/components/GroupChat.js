321eba849ecb30afaf39cfda287808cd44a575ec
CodeKing <jahswill.ayokunle@gmail.com>
2026-04-24 16:30:22 +0100

    Create GroupChat component for group messaging

---
+++ src/components/GroupChat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GroupChat = () => {
+    return (
+        <div>
+            <h1>Group Chat</h1>
+            <p>This is a component for group messaging.</p>
+        </div>
+    );
+};
+
+export default GroupChat;
